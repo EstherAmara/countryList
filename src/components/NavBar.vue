110e58876082c38a413e5b36349ca404a0ce37be
Esther Amara <esthere.amara@gmail.com>
2020-08-14 14:04:45 +0100

    country choice

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
     <nav class="navbar navbar-expand-lg navbar-light bg-info">
-        <div class="navbar-brand text-white"> Country List </div>
+        <div class="navbar-brand text-white"> Country Choice </div>
     </nav>
 </template>
